94d1034eb25d52cc48dd114a9a95b1f8c241749b
James Freeman <james@mediashakers.com>
2013-10-06 23:10:08 -0400

    Added random language picker

---
+++ language_picker.php
@@ -0,0 +1,18 @@
+<?php
+//this will just pick out a random language for me to complete the challenge in
+//some of these I have little to 0 experience with, so more learning for me
+
+$languages = array
+(
+	'PHP',
+	'Ruby',
+	'Python',
+	'Bash',
+	'Javascript',
+	'Java',
+	'Perl',
+	'C',
+	'C++',
+);
+
+echo $languages[rand(0, count($languages) - 1)];
